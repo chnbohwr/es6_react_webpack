be760a835dce751d29f58ecc9ac98a072857ff89
Hyman_Chen <hyman_chen@wistron.com>
2015-11-16 11:01:10 +0800

    add react-redux module

--- src/app.js
+++ src/app.js
@@ -1,5 +1,43 @@
 import Hello from './Hello.jsx';
-import {createStore} from 'redux';
+import {createStore, combineReducers} from 'redux';
 
-var store = createStore(()=>{});
-console.log(store);
+var userReducer = function (state = {}, action) {
+    console.log('userReducer was called with state', state, 'and action', action)
+
+    switch (action.type) {
+        case 'SET_NAME':
+            return Object.assign(state,{
+                name:action.name
+            });
+        default:
+            return state;
+    }
+}
+
+var itemReducer = (state={},action)=>{
+    console.log('itemsReducer was called with state', state, 'and action', action)
+    return state;
+};
+
+var reducer = combineReducers({
+    user:userReducer,
+    items:itemReducer
+});
+
+var store = createStore(reducer);
+
+store.subscribe(()=>{
+    console.log('store has been updated,',store.getState());
+});
+
+var setNameACtionCreator = (name)=>{
+    return{
+        type:'SET_NAME',
+        name:name
+    };
+};
+
+store.dispatch(setNameACtionCreator('jack'));
+store.dispatch(setNameACtionCreator('mick'));
+store.dispatch(setNameACtionCreator('Dell'));
+
