f595ad49e640ac5a5b2107bbf4c2366bd37892a5
Hyman_Chen <hyman_chen@wistron.com>
2015-11-16 09:11:35 +0800

    use react-dom to render instated of react

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import ReactDOM from 'react-dom';
+
 class Hello extends React.Component{
     render(){
         return <h1>Hello</h1>
     }
 }
 
-React.render(<Hello/>,document.getElementById('hello'));
+ReactDOM.render(<Hello/>,document.getElementById('hello'));
